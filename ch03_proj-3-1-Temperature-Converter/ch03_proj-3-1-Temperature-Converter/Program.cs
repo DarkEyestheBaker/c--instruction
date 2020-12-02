93ce86acabbc118a6838342b04bae6ef9406f157
DarkEyesTheBaker <julesdietsch@fioptics.com>
2020-12-01 20:23:04 -0500

    Temperature Converter

---
+++ ch03_proj-3-1-Temperature-Converter/ch03_proj-3-1-Temperature-Converter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ch03_proj_3_1_Temperature_Converter
+{
+	class Program
+	{
+		static void Main(string[] args)
+
+		{
+			Console.WriteLine("Welcome to the Temperature Converter!");
+			//while loop
+			String choice = "y";
+			while (choice.Equals("y"))
+			{
+
+				Console.Write("Enter degrees in Fahrenheit: ");
+				double fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+				double celsius = (fahrenheit - 32) * 5 / 9;
+				Console.WriteLine("Degrees in Celsius: " + celsius);
+				Console.WriteLine("Continue? (y/n)");
+				choice = Console.ReadLine();
+
+			}
+			Console.WriteLine("Good-bye!");
+		}
+	}
+}
