80e5a1c4c8260d45b37918344ea8af8903c3d262
DarkEyesTheBaker <julesdietsch@fioptics.com>
2020-12-02 13:43:50 -0500

    Travel Time Calc. Hrs/Min

--- ch03-proj-3-2-Travel-Time-Calculator/ch03-proj-3-2-Travel-Time-Calculator/Program.cs
+++ ch03-proj-3-2-Travel-Time-Calculator/ch03-proj-3-2-Travel-Time-Calculator/Program.cs
@@ -6,14 +6,14 @@ namespace ch03_proj_3_2_Travel_Time_Calculator
 	{
 		static void Main(string[] args)
 		{
-			 int speed, Decimal,  timespan;
+			double speed,  miles,  timespan;
 			Console.WriteLine("Welcome to the Travel Time Calculator!");
 			Console.WriteLine();
 
 			String choice = "y";
 			while (choice.Equals("y")) {
 				Console.Write("Enter miles: ");
-				Decimal miles = Convert.ToDecimal(Console.ReadLine());
+				miles = Convert.ToDouble(Console.ReadLine());
 				Console.WriteLine();
 
 				Console.Write("Enter miles per hour: ");
@@ -24,14 +24,14 @@ namespace ch03_proj_3_2_Travel_Time_Calculator
 
 				Console.WriteLine("Estimated travel time: ");
 				Console.WriteLine("______________________");
-				d1 = miles / speed;
+				double d1 = miles / speed;
 				var timeSpan = TimeSpan.FromMinutes(d1);
 				int hh = timeSpan.Hours;
 				int mm = timeSpan.Minutes;
 
 				Console.WriteLine("Hours: " + hh);
 				Console.WriteLine("Minutes: " + mm);
-				Console.WriteLine("Continue?  (y/n)");
+				Console.Write("Continue?  (y/n)");
 				choice = Console.ReadLine();
 			}
 			Console.WriteLine("Good-bye!");
