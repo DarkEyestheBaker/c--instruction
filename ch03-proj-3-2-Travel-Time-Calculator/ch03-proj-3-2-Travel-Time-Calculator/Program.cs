f3e4fbfdbc1b11f308b051dc7f359727f7d61b49
DarkEyesTheBaker <julesdietsch@fioptics.com>
2020-12-02 14:53:38 -0500

    Travel Time Calculator

--- ch03-proj-3-2-Travel-Time-Calculator/ch03-proj-3-2-Travel-Time-Calculator/Program.cs
+++ ch03-proj-3-2-Travel-Time-Calculator/ch03-proj-3-2-Travel-Time-Calculator/Program.cs
@@ -6,7 +6,7 @@ namespace ch03_proj_3_2_Travel_Time_Calculator
 	{
 		static void Main(string[] args)
 		{
-			double speed,  miles,  timespan;
+			double speed, miles, timeSpan;
 			Console.WriteLine("Welcome to the Travel Time Calculator!");
 			Console.WriteLine();
 
@@ -14,25 +14,25 @@ namespace ch03_proj_3_2_Travel_Time_Calculator
 			while (choice.Equals("y")) {
 				Console.Write("Enter miles: ");
 				miles = Convert.ToDouble(Console.ReadLine());
-				Console.WriteLine();
 
 				Console.Write("Enter miles per hour: ");
 				speed = Convert.ToInt32(Console.ReadLine());
 				Console.WriteLine();
 
-				Console.WriteLine();
-
 				Console.WriteLine("Estimated travel time: ");
 				Console.WriteLine("______________________");
 				double d1 = miles / speed;
-				var timeSpan = TimeSpan.FromMinutes(d1);
-				int hh = timeSpan.Hours;
-				int mm = timeSpan.Minutes;
-
+				var timeInMinutes = TimeSpan.FromMinutes(d1);
+				var timeInHours = TimeSpan.FromHours(d1);
+				int hh = timeInHours.Hours;
+				int mm = timeInMinutes.Minutes;
+				Console.WriteLine();
 				Console.WriteLine("Hours: " + hh);
 				Console.WriteLine("Minutes: " + mm);
-				Console.Write("Continue?  (y/n)");
+				Console.WriteLine();
+				Console.Write("Continue?  (y/n) ");
 				choice = Console.ReadLine();
+				Console.WriteLine();
 			}
 			Console.WriteLine("Good-bye!");
 		}
