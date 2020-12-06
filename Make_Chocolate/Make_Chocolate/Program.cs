0a5b882d62312ad6ed6cfa0dd020fc8e63d88bae
DarkEyesTheBaker <julesdietsch@fioptics.com>
2020-12-06 18:21:35 -0500

    Make Chocolate and Table of Powers

---
+++ Make_Chocolate/Make_Chocolate/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Make_Chocolate
+{
+	class Program
+	{
+		public static int MakeChocolate(int small, int big, int goal)
+		{
+			if (small + (big * 5) < goal)
+			{
+				return -1;
+			}
+			else
+			{
+				for (int i = 0; i < big; i++) // int i = counter
+				{
+					if (goal >= 5)
+					{
+						goal -= 5;
+					}
+
+				}
+				if (goal > small)
+				{
+					return -1;
+
+				}
+				else
+				{
+					return goal;
+				}
+			}
+
+
+
+		}
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine(MakeChocolate(4, 1, 9));
+			Console.WriteLine(MakeChocolate(4, 1, 10));
+			Console.WriteLine(MakeChocolate(4, 1, 7));
+
+
+
+
+
+		}
+
+	}
+}
---
+++ ch04-prj_4-1_Table_of_Powers/ch04-prj_4-1_Table_of_Powers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ch04_prj_4_1_Table_of_Powers
+{
+	class Program
+	{
+
+	
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to the Squares and Cubes Table!");
+			String choice = "y";
+			while (choice.Equals("y"))
+			{
+
+				int n = GetInt("Enter an integer: ");
+				Console.WriteLine("Number\tSquared\tCubed");
+				Console.WriteLine ("======\t=======\t=====");
+				for (int i = 1; i <= n; i++) 
+				{
+					Console.WriteLine(i + "\t" +(i*i) + "\t" +(i*i*i));
+
+				}
+				Console.WriteLine();
+
+				Console.Write("Continue?  (y/n) ");
+				choice = Console.ReadLine();
+				Console.WriteLine();
+
+
+
+			}
+			Console.WriteLine("Bye!");
+		}
+		private static int GetInt(String prompt) {
+			int n = 0;
+			Console.Write(prompt);
+			n = int.Parse(Console.ReadLine());
+			Console.WriteLine();
+			return n;
+		}
+	}
+}
